99112b8e184d4559d88a63e3d55095405ad806eb
scottrpoulin <spoulin@unomaha.edu>
2021-03-18 11:24:15 -0600

    add repo to github trigger

--- components/setup_github_trigger.sh
+++ components/setup_github_trigger.sh
@@ -6,8 +6,8 @@
 # In order to try this out, fork this repo into your personal GitHub account
 # Then, change the repo-owner to be your GitHub id
 
-REPO_NAME=training-data-analyst
-REPO_OWNER=GoogleCloudPlatform
+REPO_NAME=gcp-mlops-demo
+REPO_OWNER=scottrpoulin
 
 #for trigger_name in trigger-000 trigger-001 trigger-002 trigger-003; do
 #  gcloud beta builds triggers delete --quiet $trigger_name
@@ -19,7 +19,7 @@ create_github_trigger() {
     gcloud beta builds triggers create github \
       --build-config="${DIR_IN_REPO}/cloudbuild.yaml" \
       --included-files="${DIR_IN_REPO}/**" \
-      --branch-pattern="^master$" \
+      --branch-pattern="^main" \
       --repo-name=${REPO_NAME} --repo-owner=${REPO_OWNER} 
 }
 
