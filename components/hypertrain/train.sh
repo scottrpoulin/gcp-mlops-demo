b4a5d1cce7cddacc802c43913f9e423f469a454c
scottrpoulin <spoulin@unomaha.edu>
2021-03-22 14:11:52 -0600

    fix package path

--- components/hypertrain/train.sh
+++ components/hypertrain/train.sh
@@ -23,7 +23,7 @@ gsutil -m rm -rf $OUTDIR || true
 gcloud ai-platform jobs submit training $JOBNAME \
   --region=$REGION \
   --module-name=trainer.task \
-  --package-path=/babyweight/src/babyweight/trainer \
+  --package-path=/babyweight/src/gcp-mlops-demo/babyweight/trainer \
   --job-dir=$OUTDIR \
   --staging-bucket=gs://$BUCKET \
   --scale-tier=STANDARD_1 \
