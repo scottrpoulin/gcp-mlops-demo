6a42b0ca6905dc9c80df42c9f437c658c748e6e8
scottrpoulin <spoulin@unomaha.edu>
2021-03-22 12:41:53 -0600

    update traintuned

--- components/traintuned/train.sh
+++ components/traintuned/train.sh
@@ -11,15 +11,16 @@ HYPERJOB=$1
 BUCKET=$2
 TFVERSION=1.8
 REGION=us-central1
+PROJECT=$3
 
 echo "Extracting information for job $HYPERJOB"
 
 # get information from the best hyperparameter job
-RMSE=$(gcloud ai-platform jobs describe $HYPERJOB --format 'value(trainingOutput.trials.finalMetric.objectiveValue.slice(0))')
-NNSIZE=$(gcloud ai-platform jobs describe $HYPERJOB --format 'value(trainingOutput.trials.hyperparameters.nnsize.slice(0))')
-BATCHSIZE=$(gcloud ai-platform jobs describe $HYPERJOB --format 'value(trainingOutput.trials.hyperparameters.batch_size.slice(0))')
-NEMBEDS=$(gcloud ai-platform jobs describe $HYPERJOB --format 'value(trainingOutput.trials.hyperparameters.nembeds.slice(0))')
-TRIALID=$(gcloud ai-platform jobs describe $HYPERJOB --format 'value(trainingOutput.trials.trialId.slice(0))')
+RMSE=$(gcloud ai-platform jobs describe $HYPERJOB --project $PROJECT --format 'value(trainingOutput.trials.finalMetric.objectiveValue.slice(0))')
+NNSIZE=$(gcloud ai-platform jobs describe $HYPERJOB --project $PROJECT --format 'value(trainingOutput.trials.hyperparameters.nnsize.slice(0))')
+BATCHSIZE=$(gcloud ai-platform jobs describe $HYPERJOB --project $PROJECT --format 'value(trainingOutput.trials.hyperparameters.batch_size.slice(0))')
+NEMBEDS=$(gcloud ai-platform jobs describe $HYPERJOB --project $PROJECT --format 'value(trainingOutput.trials.hyperparameters.nembeds.slice(0))')
+TRIALID=$(gcloud ai-platform jobs describe $HYPERJOB --project $PROJECT --format 'value(trainingOutput.trials.trialId.slice(0))')
 
 echo "Continuing to train model in $TRIALID with nnsize=$NNSIZE batch_size=$BATCHSIZE nembeds=$NEMBEDS"
 
