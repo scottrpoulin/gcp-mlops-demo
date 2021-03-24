e602565cddb1c0ef78754b3e980703e74ec6e4c9
scottrpoulin <spoulin@unomaha.edu>
2021-03-23 21:27:31 -0600

    deploy fix hopefully

--- application/main.py
+++ application/main.py
@@ -43,7 +43,6 @@ def get_prediction(features):
                 'instances': [features]}
   parent = 'projects/%s/models/%s/versions/%s' % (project, model_name, version_name)
   prediction = api.projects().predict(body=input_data, name=parent).execute()
-  print(prediction)
   return prediction['predictions'][0]
 
 
